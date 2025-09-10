6e9961875007d6cf0f27f81104232b4bcc4fcb75
Md. Tasnimur Rahman Shakir <tasnim.bd.cse@gmail.com>
2025-09-10 22:43:20 +0600

    Update AboutSection.jsx

--- src/Components/AboutSection.jsx
+++ src/Components/AboutSection.jsx
@@ -52,7 +52,7 @@ export const AboutSection = () => {
                 Get In Touch
               </a>
               <a
-                href="../assets/Tasnimur_Rahman_Resume.pdf"
+                href="/Tasnimur_Rahman_Resume.pdf"
                 download="Md. Tasnimur Rahman Resume.pdf"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/30 transition-colors duration-300"
               >
