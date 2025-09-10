2966496636c99bdcf30884075dc56d57e77952be
Md. Tasnimur Rahman Shakir <tasnim.bd.cse@gmail.com>
2025-09-10 21:24:41 +0600

    Update sendEmail.js

--- src/api/sendEmail.js
+++ src/api/sendEmail.js
@@ -2,7 +2,7 @@ export default async function handler(req, res) {
     if (req.method !== "POST") {
         return res.status(405).json({ message: "Method not allowed" });
     }
-
+    console.log(req.body)
     const { name, email, message } = req.body;
 
     try {
